5593b268e80931844defa176e750cafc00dbdad9
Maciej Kozłowski <maciej.kozlowski@espeo.eu>
2020-01-20 16:24:08 +0100

    Created view pager extensions

---
+++ androidutils/src/main/java/eu/espeo/androidutils/extensions/ViewPagerExtensions.kt
@@ -0,0 +1,29 @@
+package eu.espeo.androidutils.extensions
+
+import androidx.viewpager.widget.ViewPager
+
+/**
+ * Created by Maciej Kozłowski on 2020-01-20.
+ */
+
+typealias PageSelectedListener = (Int) -> Unit
+
+fun ViewPager.addPageSelectedListener(onPageSelected: PageSelectedListener) {
+    this.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+        override fun onPageScrollStateChanged(state: Int) {
+            // Nothing to do
+        }
+
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+            // Nothing to do
+        }
+
+        override fun onPageSelected(position: Int) {
+            onPageSelected(position)
+        }
+    })
+}
